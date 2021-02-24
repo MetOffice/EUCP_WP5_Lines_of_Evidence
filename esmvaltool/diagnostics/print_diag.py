aa1c09192323817f72dd3b7a06cacdde558fcdc7
thomas.crocker@metoffice.gov.uk <thomas.crocker@metoffice.gov.uk>
2021-02-24 16:32:20 +0000

    changed print diag to output diff info

--- esmvaltool/diagnostics/print_diag.py
+++ esmvaltool/diagnostics/print_diag.py
@@ -1,7 +1,8 @@
 # quick diagnostic for printing output from pre-processor
-from esmvaltool.diag_scripts.shared import run_diagnostic
-
-import iris
+from esmvaltool.diag_scripts.shared import (
+    run_diagnostic,
+    group_metadata,
+)
 
 import os
 import logging
@@ -11,12 +12,20 @@ logger = logging.getLogger(os.path.basename(__file__))
 
 def main(cfg):
     # The config object is a dict of all the metadata from the pre-processor
-    logger.info(cfg)
+    logger.debug(cfg)
 
-    for f in cfg["input_data"].keys():
-        cube = iris.load_cube(f)
-        logger.info(f)
-        logger.info(cube)
+    projects = group_metadata(cfg["input_data"].values(), "project")
+
+    for k, p in projects.items():
+        m_list = set()
+        for ds in p:
+            if k == "CORDEX":
+                ds_str = f"{ds['driver']} - {ds['dataset']}"
+            else:
+                ds_str = ds["dataset"]
+            m_list.add(ds_str)
+        print(f"{k} - {len(m_list)} models:")
+        print(m_list)
 
 
 if __name__ == "__main__":
